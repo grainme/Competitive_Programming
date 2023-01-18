3f177c109a9c4fb463fbfe4b3dd2a361ff62adfe
Marouane Boufarouj <104838272+MarouaneBouf@users.noreply.github.com>
2023-01-18 17:24:50 +0100

    Create code.cpp

---
+++ CodeLand_100_IEEE/Couscous Dilemma/code.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+using ll = long long;
+ 
+void solve()
+{
+  int T;
+  cin >> T;
+  cout << (T%2==0 && T!=2?"YES":"NO") << endl;
+}
+ 
+int main() 
+{
+  solve();
+  return 0;
+}
