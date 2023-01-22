ede4604e1b5f8a14b61b48179c70c799b8bffc3b
Marouane Boufarouj <104838272+MarouaneBouf@users.noreply.github.com>
2023-01-22 22:00:00 +0100

    Update code.cpp

--- LeetCode/sum-of-unique-elements/code.cpp
+++ LeetCode/sum-of-unique-elements/code.cpp
@@ -16,3 +16,28 @@ public:
         return s;    
     }
 };
+
+/*
+
+Efficient Code using HashTable
+
+
+class Solution {
+public:
+    int sumOfUnique(vector<int>& nums)
+    {
+        unordered_map<int, int> visited;
+        int s = 0;
+        for(auto &i: nums)
+        {
+            visited[i]++;
+        }
+        for(auto &i: nums)
+        {
+            if(visited[i] == 1){s+=i;}
+        }
+        return s;
+    }
+};
+
+*/
