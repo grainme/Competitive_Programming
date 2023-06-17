a6e9a498eb049b4d222fb3cdf37965deff27c7f3
Marouane Boufarouj <104838272+MarouaneBouf@users.noreply.github.com>
2023-06-18 00:26:58 +0100

    Update code.cpp
    Push Missing Coin Sum Problem from CSES.fi

--- CSES/Missing coin sum/code.cpp
+++ CSES/Missing coin sum/code.cpp
@@ -1,1 +1,43 @@
-// Test
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // Read the number of coins
+    int n;
+    cin >> n;
+
+    // Create a vector to store the coin values
+    vector<int> coins(n);
+
+    // Read the coin values and store them in the vector
+    for (int i = 0; i < n; i++) {
+        cin >> coins[i];
+    }
+
+    // Sort the coin values in ascending order
+    sort(coins.begin(), coins.end());
+
+    // Initialize the answer to 1
+    int ans = 1;
+
+    // Iterate through the sorted coin values
+    for (int i = 0; i < n; i++) {
+        // If the current coin value is less than or equal to the answer,
+        // it means we can form the sum of 'ans + coins[i]'
+        if (coins[i] <= ans) {
+            // Add the coin value to the answer to update it
+            ans += coins[i];
+        } else {
+            // If the current coin value is greater than the answer,
+            // it means we have found the smallest sum that cannot be created.
+            // Since 'coins' is sorted in ascending order, if 'coins[i]' is greater
+            // than 'ans', we won't be able to form the sum 'ans' using the previous
+            // coins as well. Therefore, we break out of the loop.
+            break;
+        }
+    }
+
+    // Print the smallest sum that cannot be created
+    cout << ans << endl;
+
+}
